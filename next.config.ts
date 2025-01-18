d17d1658130bbabf59c6f5f13bf56f11e6eb0e16
Muhammad Junaid Khan <160384541+junaidkhen@users.noreply.github.com>
2025-01-18 23:09:20 +0500

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  // Add other config options here
+};
+
+export default nextConfig;
+
